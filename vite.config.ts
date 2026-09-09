86780d268a83297eba7c876a72e7c8be30b44bce
Eliseu Nogueira <adm@eliseunogueira.com.br>
2026-09-09 02:12:28 -0300

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // <-- Adicione esta linha
+const base = process.env.NODE_ENV === 'production'
+    ? '/englishsoulv2/'
+    : '/';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), // <-- Adicione esta linha
   ],
-  base:'/englishsoulv2/',
+  base,
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 })
